18e7244e92f6eb422adb319f3be8df53a2e267c9
Jessa Mae Loresto <jam.loresto@gmail.com>
2025-04-10 21:07:20 +0800

    test commit

--- server/app.js
+++ server/app.js
@@ -3,8 +3,6 @@ const path = require("path");
 const helmet = require("helmet");
 const cors = require("cors");
 
-// import { connectToDatabase } from "./config/db";
-
 const app = express();
 
 app.use(
@@ -25,25 +23,11 @@ app.get("/api/hello", (req, res) => {
 	res.json({ message: "Hello from the backend!" });
 });
 
-// app.use("/api/rsvp", rsvpRoutes);
-
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../public_html/dist/index.html"));
 });
 
-// connectToDatabase()
-// 	.then(() => {
-// 		const PORT = process.env.PORT || 3000;
-// 		app.listen(PORT, () => {
-// 			console.log(`Server running on http://localhost:${PORT}`);
-// 		});
-// 	})
-// 	.catch((error) => {
-// 		console.error("Failed to connect to the database.", error);
-// 	});
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
