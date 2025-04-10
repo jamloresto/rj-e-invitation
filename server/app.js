3773fd6d88fe93ffd4889b14c6d625c077be542a
Jessa Mae Loresto <jam.loresto@gmail.com>
2025-04-10 21:18:51 +0800

    test remove hello

--- server/app.js
+++ server/app.js
@@ -19,10 +19,6 @@ app.use(helmet());
 
 app.use(express.static(path.join(__dirname, "../public_html/dist")));
 
-app.get("/api/hello", (req, res) => {
-	res.json({ message: "Hello from the backend!" });
-});
-
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../public_html/dist/index.html"));
 });
