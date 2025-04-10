5ee781ff9127bbb03bd1141409c3a5a505f6d7da
Jessa Mae Loresto <jam.loresto@gmail.com>
2025-04-10 20:12:32 +0800

    test change route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,32 +2,26 @@ import {
 	BrowserRouter as Router,
 	Routes,
 	Route,
-	Navigate,
 } from "react-router-dom";
 
 import Gallery from "./pages/Gallery";
 
 const App = () => {
 	return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Navigate
-              to="/gallery"
-              replace
-            />
-          }
-        />
-        <Route
-          path="/gallery"
-          element={<Gallery />}
-        />
+		<Router>
+			<Routes>
+				<Route
+					path="/"
+					element={<Gallery />}
+				/>
+				<Route
+					path="/gallery"
+					element={<Gallery />}
+				/>
 
-        {/* <Route path="/rsvp/*" element={<Rsvp />} /> */}
-      </Routes>
-    </Router>
+				{/* <Route path="/rsvp/*" element={<Rsvp />} /> */}
+			</Routes>
+		</Router>
 	);
 };
 export default App;
